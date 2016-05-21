bdac03cee56ab7cc7ccc176480b72747e99a70dc
xome4ok <dmitriy.pryanichnikov@gmail.com>
2016-05-22 03:18:49 +0500

    little cleanup

--- Jackal/App.xaml.cs
+++ Jackal/App.xaml.cs
@@ -1,13 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using Newtonsoft.Json;
-using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace Jackal
