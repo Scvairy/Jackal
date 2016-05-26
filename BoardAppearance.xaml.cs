e4b6305d78c43f1a2c79fa56d0ffdd3bdbf08f14
Scvairy <scvairy@gmail.com>
2016-05-26 16:20:19 +0500

    Вынос игрового поля в отдельный usercontrol

---
+++ BoardAppearance.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Jackal
+{
+    /// <summary>
+    /// Логика взаимодействия для BoardAppearance.xaml
+    /// </summary>
+    public partial class BoardAppearance : UserControl
+    {
+        public BoardAppearance()
+        {
+
+            InitializeComponent();
+            Board Board = new Board();
+
+            DrawTiles(Board); // Draw all tiles on grid. To draw current state of board, simply call this method
+
+        }
+
+        void DrawTiles(Board board) // draws all tiles of given board on grid. To draw current state of board, simply call this method
+        {
+            var tileImages = board.GetBitmapImages();
+
+            for (var y = 0; y < tileImages.GetLength(1); y++)
+            {
+                for (var x = 0; x < tileImages.GetLength(0); x++)
+                {
+                    var currentImageToShow = new Image();
+                    var src = tileImages[x, y];
+                    currentImageToShow.Source = src;
+                    currentImageToShow.Stretch = System.Windows.Media.Stretch.Uniform;
+                    grid.Children.Add(currentImageToShow); // add image to grid
+                    Grid.SetColumn(currentImageToShow, x); // move it to appropriate column
+                    Grid.SetRow(currentImageToShow, y);  // and row
+                }
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,34 +8,9 @@ namespace Jackal
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int w = 50, h = 500;
         public MainWindow()
         {
-            SizeToContent = SizeToContent.WidthAndHeight; // fits window size to content size
             InitializeComponent();
-            Board Board = new Board();
-                        
-            DrawTiles(Board); // Draw all tiles on grid. To draw current state of board, simply call this method
-
-        }
-
-        void DrawTiles(Board board) // draws all tiles of given board on grid. To draw current state of board, simply call this method
-        {
-            var tileImages = board.GetBitmapImages();
-
-            for (var y = 0; y < tileImages.GetLength(1); y++)
-            {
-                for (var x = 0; x < tileImages.GetLength(0); x++)
-                {
-                    var currentImageToShow = new Image();
-                    var src = tileImages[x, y];
-                    currentImageToShow.Source = src;
-                    currentImageToShow.Stretch = System.Windows.Media.Stretch.Uniform;
-                    grid.Children.Add(currentImageToShow); // add image to grid
-                    Grid.SetColumn(currentImageToShow, x); // move it to appropriate column
-                    Grid.SetRow(currentImageToShow, y);  // and row
-                }
-            }
         }
     }
 }
